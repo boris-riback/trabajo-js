081fc6878d75e15c2f148eef8ceab18ce52b0488
boris-riback <briback@ribackcereales.com>
2024-09-30 21:52:59 -0300

    agregado de carrito con arrays

--- main.js
+++ main.js
@@ -1,38 +1,37 @@
 
-//declaracion de variables
+//declaracion de arrays y objetos
 
-function Articulo (nombre, precio, stock)
-{
+const carrito = [];
+class Articulo {
+  constructor(nombre, precio, stock, stockCompra, subtotal){
     this.nombre = nombre;
     this.precio = precio;
     this.stock = stock;
+    this.stockCompra = stockCompra;
+    this.subtotal = subtotal;
+  }
+  informacion () {
+    return "Disponemos de "+this.stock +" Kg de "+this.nombre+" a un precio de "+ this.precio +"$";
+  }
 };
 
-const malta = new Articulo ("malta", 3000, 2000);
-const levadura = new Articulo ("levadura", 2000, 1500);
-const lupulo = new Articulo ("lupulo", 5000, 1000);
-
-//variable de precios
-let precioLupulo = 5000;
-let precioMalta = 3000;
-let precioLevadura = 2000;
 
 //articulos
+const malta = new Articulo ("malta", 3000, 2000, 0, 0);
+const levadura = new Articulo ("levadura", 2000, 1500, 0, 0);
+const lupulo = new Articulo ("lupulo", 5000, 1000, 0, 0);
+
+//variables para la compra
 let nombreArticulo = "";
 let cantidad = 0;
 let totalCompra = 0;
 let compra = 0;
-
-//variables de stock
-let stockLupulo = 1000;
-let stockMalta = 2000;
-let stockLevadura = 1500;
+let mensajeDeCompra = "";
 
 //variables de validacion
-/* (en caso de que usara variables booleanas, por ejemplo,
+/* (en caso de que usara variables boolean    as, por ejemplo,
 en la variable hay stock si es false el articulo tiene stock
 y si es true el articulo no tiene stock y sonaria contradictorio y seria confuso) */
-
 let hayStock = "NS";
 let articuloValido = "NV";
 let seguirComprando = true;
@@ -49,82 +48,97 @@ nombreArticulo = prompt("Seleccione el articulo deseado (malta, levadura o lúpu
 while (seguirComprando){
 //validacion del producto
 
-    do {
-        switch (nombreArticulo) {
-            case "malta":
-                alert("Disponemos de "+malta.stock+" Kg de malta a un precio de "+malta.precio+" $");
-                articuloValido = "V";
-            break;
+  do {
+    switch (nombreArticulo) {
+      case "malta":
+        alert(malta.informacion());
+          articuloValido = "V";
+        break;
 
-            case "lupulo":
-                alert("Disponemos de "+lupulo.stock+" Kg de Lupulo a un precio de "+lupulo.precio+" $");
-                articuloValido = "V";
-            break;
+      case "lupulo":
+        alert(lupulo.informacion());
+          articuloValido = "V";
+        break;
+      case "levadura":
+        alert(levadura.informacion());
+          articuloValido = "V";
+        break;
+      default:
+        alert ("Usted ha ingresado mal el nombre del articulo");
+          articuloValido = "NV";
+          nombreArticulo = prompt ("Por favor reingrese el articulo");
+        break;}
+  } while (articuloValido == "NV");
 
-            case "levadura":
-                alert("Disponemos de "+levadura.stock+" Kg de levadura a un precio de "+levadura.precio+" $");
-                articuloValido = "V";
-            break;
+  cantidad = prompt("Ahora seleccione la cantidad");
 
-            default:
-                alert ("Usted ha ingresado mal el nombre del articulo")
-                articuloValido = "NV"
-                nombreArticulo = prompt ("Por favor reingrese el articulo")
-            break;
-            }
-    } while (articuloValido == "NV");
+//validacion de la cantidad
 
-    cantidad = prompt("Ahora seleccione la cantidad");
-
-
-        //validacion de la cantidad
-
-    while (isNaN(cantidad) || cantidad <=0) {
-        alert("Usted ingresó una cantidad invalida");
-        cantidad = prompt("Reingrese la cantidad");
-    }
+  while (isNaN(cantidad) || cantidad <=0) {
+    alert("Usted ingresó una cantidad invalida");
+    cantidad = prompt("Reingrese la cantidad");
+  };
 
 
 //comprobacion de stock y validacion de compra
 
-    while (hayStock == "NS") {
+  while (hayStock == "NS") {
 
-        if (nombreArticulo == "malta" && stockMalta >= cantidad) {
-            compra = COMPRAF (malta.precio, cantidad);
-            malta.stock -= cantidad;
-            totalCompra += compra  ;
-            alert("Producto agregado al carrito, su total es de: $" + totalCompra);
-            hayStock = "HS";
+    if (nombreArticulo == "malta" && malta.stock >= cantidad) {
+      compra = COMPRAF (malta.precio, cantidad);
+      malta.stockCompra += cantidad;
+      malta.subtotal += compra;
+      malta.stock -= cantidad;
+      totalCompra += compra;
+      alert("Producto agregado al carrito, su total es de: $" + totalCompra);
+      hayStock = "HS";
 
-        } else if (nombreArticulo == "lupulo" && stockLupulo >= cantidad) {
-            compra = COMPRAF (lupulo.precio, cantidad);
-            lupulo.stock -= cantidad;
-            totalCompra += compra;
-            alert("Producto agregado al carrito, su total es de: $" + totalCompra);
-            hayStock = "HS";
+    } else if (nombreArticulo == "lupulo" && lupulo.stock >= cantidad) {
+      compra = COMPRAF (lupulo.precio, cantidad);
+      lupulo.stockCompra += cantidad;
+      lupulo.subtotal += compra;
+      lupulo.stock -= cantidad;
+      totalCompra += compra;
+      alert("Producto agregado al carrito, su total es de: $" + totalCompra);
+      hayStock = "HS";
 
-        } else if (nombreArticulo == "levadura" && stockLevadura >= cantidad) {
-            compra = COMPRAF (levadura.precio, cantidad);
-            levadura.stock -= cantidad;
-            totalCompra += compra;
-            alert("Producto agregado al carrito, su total es de: $" + totalCompra);
-            hayStock = "HS";
-        }else {
-            alert("No hay suficiente stock de este producto, intente con una cantidad menor");
-            cantidad = prompt("Por favor seleccione una cantidad menor");
-        }
+    } else if (nombreArticulo == "levadura" && levadura.stock >= cantidad) {
+      compra = COMPRAF (levadura.precio, cantidad);
+      levadura.stockCompra += cantidad;
+      levadura.subtotal += compra;
+      levadura.stock -= cantidad;
+      totalCompra += compra;
+      alert("Producto agregado al carrito, su total es de: $" + totalCompra);
+      hayStock = "HS";
+
+    }else {
+      alert("No hay suficiente stock de este producto, intente con una cantidad menor");
+      cantidad = prompt("Por favor seleccione una cantidad menor");
     }
 
+//agregado de productos al carrito (Array)
+    if (malta.stockCompra > 0) {
+      carrito.push (malta);
+    }else if (lupulo.stockCompra > 0) {
+      carrito.push (lupulo);
+    }else if (levadura.stockCompra > 0)
+      carrito.push (levadura);
+  };
 
 //reiniciar o finalizar el bucle para terminar con la compra
 
     seguirComprando = confirm("Desea agregar mas productos a su carrito?");
 
     if (seguirComprando) {
-        nombreArticulo = prompt ("Seleccione otro articulo");
-        hayStock = "NS";
-        articuloValido = "V"
+      nombreArticulo = prompt ("Seleccione otro articulo");
+      hayStock = "NS";
+      articuloValido = "V"
     }else {
-        alert("Gracias por su compra!!, el total de la compra es de "+totalCompra+"$");
-    }
-}
+      for (let i = 0; i < carrito.length; i++) {
+        mensajeDeCompra += `${carrito[i].nombre} x ${carrito[i].stockCompra} kg= ${carrito[i].subtotal}$\n`;
+      }
+      alert(mensajeDeCompra);
+      alert("Gracias por su compra!!, el total de la compra es de "+totalCompra+"$");
+      }
+    };
+
